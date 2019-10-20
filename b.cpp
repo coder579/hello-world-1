fc334a210edecc27a47399ae0f3985bc9e559ae7
Shubham Singh Manhas <shubhammanhas579@gmail.com>
2019-10-20 18:48:07 +0530

    CF 1237 Problem B

--- b.cpp
+++ b.cpp
@@ -1,43 +1,44 @@
-// knights on a polygon table
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-bool compare(const pair< pair<ll, ll>,ll>&i, const pair< pair<int, int>,ll>&j)
-{
-    return i.first.second < j.first.second;
-}
+typedef vector<int> vi;
+typedef pair<int,int> pi;
 
 int main()
 {
-  ll n,k,i,sum=0,j=0;
-  cin>>n>>k;
-  ll p[n],c[n];
-  pair< pair <ll,ll> , ll > knight[n];
-  for(i=0;i<n;i++) cin>>p[i];
-  for(i=0;i<n;i++) cin>>c[i];
-  for(i=0;i<n;i++)
-  {
-    knight[i] = make_pair(make_pair(p[i],c[i]),i);
-    knight1[i] = make_pair(make_pair(p[i],c[i]),i)
-  }
-  sort(knight , knight+n);
-  sort(knight1 , knight1+n, compare);
-  for(i=0;i<n;i++)
-  {
-    if(i<=k)
-    {
-      sum+=knight[i].first.second;
-    }
-    else
-    {
-      sum+=knight[i].first.second;
-      for(i=0;i<)
-      sum-=knight[i-k-1].first.second;
-    }
-    c[knight[i].second]=sum;
-  }
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int n;
+    cin>>n;
 
-  for(i=0;i<n;i++) cout<<c[i]<<" ";
-  cout<<endl;
+    int inp[n],oup[n];
 
+   for(int i=0;i<n;i++) cin>>inp[i];
+   for(int i=0;i<n;i++) cin>>oup[i]; 
+
+   int vis[n+1];
+   for(int i=0;i<=n;i++) vis[i]=0;
+    
+   int ans=0,c1=0,c2=0;
+   while(true){
+       if(c1==n || c2==n) break;
+
+       if(inp[c1]==oup[c2]){
+          c1++;c2++; 
+          vis[oup[c2]]=1;
+       }
+      else if(vis[inp[c1]]){
+          c1++;
+      } 
+       else {
+          if(vis[oup[c2]]==0){
+              ans++;
+          } 
+         vis[oup[c2]]=1; 
+          c2++;
+       }
+   }
+    cout<<ans; 
+    return 0;
 }
